f0ef626897f1f98da025a4e2c86eaa99810cbc63
Patrick <patrick.pusswald@gmail.com>
2023-01-17 00:07:33 +0100

    container done maybe mistake in animation

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,13 +1,38 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.css']
+  styleUrls: ['./container.component.css'],
+  animations: [
+    
+    trigger('move', [
+      state('inMiddle', style({
+        opacity: '1',
+    })),
+    transition('* => void', [
+      animate(100, style({
+        opacity: '0',
+      }))
+    ]),
+    transition('void => *', [
+      animate(401),
+      style({
+        opacity: '0',
+        visibility: 'visible'
+      }),
+      animate(200)
+    ]),
+    
+  ]), 
+]
+
 })
 export class ContainerComponent implements OnInit {
 
   @Input() page: number = 0;
+  currentTime = Date.now();
 
   @HostListener('wheel', ['$event'])
   onMouseWheel(event: WheelEvent) {
@@ -23,16 +48,22 @@ export class ContainerComponent implements OnInit {
   }
 
   next(): void {
-    this.page++;
-    if(this.page > 3) {
-      this.page = 0;
+    if((Date.now() - this.currentTime) > 1000) {
+      this.page++;
+      if(this.page > 3) {
+        this.page = 0;
+      }
+      this.currentTime = Date.now();
     }
   }
 
   back(): void {
-    this.page--;
-    if(this.page < 0) {
-      this.page = 3;
+    if((Date.now() - this.currentTime) > 1000) {
+      this.page--;
+      if(this.page < 0) {
+        this.page = 3;
+      }
+    this.currentTime = Date.now();
     }
   }
 
